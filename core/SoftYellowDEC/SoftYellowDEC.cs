7514db28b55fa348b92344cd4dbd941464508db8
girlnamedluna <mossmossstudios@gmail.com>
2024-05-27 05:11:19 -0400

    Create SoftYellowDEC.cs

---
+++ core/SoftYellowDEC/SoftYellowDEC.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+
+namespace SoftYellowDEC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your key: ");
+            string key = Console.ReadLine();
+                if(string.IsNullOrEmpty(key))
+            {
+                Console.WriteLine("Oops! It seems you didn't enter anything in!\nPress Enter to close the program and try again!");
+                Console.ReadLine();
+            }
+
+            string folderNameData2 = "GreenBoxExport";
+            string fileName2 = "GreenBoxExport.txt";
+            string directoryPath2 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), folderNameData2);
+            string filePath2 = Path.Combine(directoryPath2, fileName2);
+
+            byte[] encryptedBytes = File.ReadAllBytes(filePath2);
+            string decryptedText = DecryptedStringFromBytes_Aes(encryptedBytes, key);
+            Console.WriteLine("Crypted text is: " +  decryptedText);
+        }
+        private static string DecryptedStringFromBytes_Aes(byte[] cipherText, string key)
+        {
+            string folderNameData2 = "GreenBoxExport";
+            string fileName2 = "GreenBoxExport.txt";
+            string directoryPath2 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), folderNameData2);
+            string filePath2 = Path.Combine(directoryPath2, fileName2);
+            string plaintext = null;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+                aesAlg.Mode = CipherMode.CBC;
+                aesAlg.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    plaintext = srDecrypt.ReadToEnd();
+                }
+            }
+            File.WriteAllText(filePath2, plaintext);
+            return plaintext;
+        }
+    }
+}
